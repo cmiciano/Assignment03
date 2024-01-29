c7796d7f02701d139607a01f604c61dcdb067141
cmiciano <cmiciano@ucsd.edu>
2024-01-28 18:39:45 -0800

    added prompts

--- js/03-triangle.js
+++ js/03-triangle.js
@@ -1,3 +1,19 @@
+/*
+
+Looping a Triangle
+
+Write a loop that displays the following triangle within a console window:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+
 let pyrRow = "#";
 for (let i = 0; i < 7; i++ ) {
     console.log(pyrRow)
